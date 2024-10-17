c67c7f724a5dae4f381c2f21f8f787ef7cda39c3
oscar <oscar.burman005@gmail.com>
2024-10-17 15:04:47 +0200

    Simple add to display functionality

--- advancedCalc.js
+++ advancedCalc.js
@@ -1,1 +1,34 @@
-//!Enkel kalkylato
+//!Advanced
+const display = document.querySelector(".display");
+const button = document.getElementById("btn-1");
+
+const operators = document.querySelectorAll(".btn-operator");
+const numbers = document.querySelectorAll(".btn-numbers");
+
+const equals = document.querySelector(".btn-equals");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", function (e) {
+    addToScreen(operator.textContent);
+  });
+});
+
+numbers.forEach((number) => {
+  number.addEventListener("click", function (e) {
+    addToScreen(number.textContent);
+  });
+});
+
+function addToScreen(input) {
+  display.value += input;
+}
+
+function equals() {
+  let num1 = document.querySelector(".display").value;
+  let num2 = math.evaluate(num1);
+  display.value = num2;
+}
+
+function ac() {
+  display.value = "";
+}
