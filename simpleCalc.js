0df5c1d899bbf0cbc7325edf4780656f526c15f0
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2024-10-18 10:13:22 +0200

    kinda done with calc

--- simpleCalc.js
+++ simpleCalc.js
@@ -1,4 +1,35 @@
-tal1 = Number(prompt("Välj första talet"))
-tal2 = Number(prompt("Välj andra talet"))
-operator = (prompt("+,-,*,/,%"))
-if 
+const tal1 = Number(prompt("Välj första talet"))
+const tal2 = Number(prompt("Välj andra talet"))
+const operator = (prompt("Välj en operator(+,-,*,/,%)"))
+if (isNaN(tal1)||isNaN(tal2)){
+    console.log("skriv in ett nummer dumfan")
+}
+ else if (operator === "+"){
+    console.log(tal1 + tal2)
+    alert(tal1 +  tal2)
+} else if (operator === "-"){
+    console.log(tal1 - tal2)
+    alert(tal1 -  tal2)
+} else if (operator === "*"){
+    console.log(tal1 * tal2)
+    alert(tal1 *  tal2) 
+    
+} else if (operator === "/"){
+    if (tal2 == 0) {
+        alert("du kan icke dela med noll")
+        console.log("du kan icke dela med noll")
+     } else {
+        console.log(tal1 / tal2)
+        alert(tal1 /  tal2)
+     }
+  
+} else if(operator === "%"){
+    if(tal2 ==0) {
+        console.log("tal2 kan inte va noll")
+        alert("tal2 kan va noll")
+    } else {
+        console.log(tal1 %  tal2)
+        alert(tal1 %  tal2)
+    }
+}
+
