47a3db391ea273e1ab9ce6876e6d6e9d0eee9e6c
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2024-10-18 10:56:04 +0200

    Done calc

--- simpleCalc.js
+++ simpleCalc.js
@@ -1,10 +1,20 @@
+let kalkylator = true
+
+
+while(kalkylator) {
 const tal1 = Number(prompt("Välj första talet"))
 const tal2 = Number(prompt("Välj andra talet"))
 const operator = (prompt("Välj en operator(+,-,*,/,%)"))
+
 if (isNaN(tal1)||isNaN(tal2)){
     console.log("skriv in ett nummer dumfan")
-}
- else if (operator === "+"){
+    alert("skriv in ett nummer dumfan")
+}else if (!["+", "-", "*", "/", "%"].includes(operator)) {
+        console.log("Skriv in en giltig operator.");
+        alert("Skriv in en giltig operator.");
+     
+    
+}else if (operator === "+"){
     console.log(tal1 + tal2)
     alert(tal1 +  tal2)
 } else if (operator === "-"){
@@ -32,4 +42,9 @@ if (isNaN(tal1)||isNaN(tal2)){
         alert(tal1 %  tal2)
     }
 }
-
+let användarinfo = prompt("vill du fortsätta uträkningen? ja/nej").toLowerCase();
+if (användarinfo !== "ja"){
+    kalkylator=false;
+    console.log("Tack för att du använde dig av team umeås kalkylator")
+}
+}
