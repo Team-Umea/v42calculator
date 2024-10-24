0d431815b04d476b606ec91c68ef38bec9f36e07
Frank Norqvist <168377759+FrankNorqv@users.noreply.github.com>
2024-10-24 21:05:22 +0200

    xoxo

--- frank.js
+++ frank.js
@@ -1,21 +1,40 @@
  menu = Number(prompt("Välj vad du vill räkna ut 1-6: 1: Addition(+) 2: Subtraktion(-)3: Multiplikation(*) 4:Division(/) 5:Exponentiering(^) 6:Modulus (%)"))
-
+let kalkylator = true
  switch (menu){
      case 1:
+
         num1 = Number(prompt("ange ditt första tal?"))
         num2 = Number(prompt("ange ditt andra tal?"))
-        console.log("ditt tal blir",num1 + num2) 
+        console.log("x + y = :",num1 + num2) 
         break;
         case 2:
-            distance = prompt("hur långt sprang du idag i kilometer?")
-            console.log( * 0.621371)
+            num1 = Number(prompt("ange ditt första tal(ex x-y )?"))
+            num2 = Number(prompt("ange ditt andra tal?"))
+            console.log("x - y = :",num1 - num2)
             break;
             case 3:
-                weight=Number(prompt("Vad väger du i kg idag?"))
-                console.log(weight * 2.20462)
+                num1 = Number(prompt("ange ditt första tal?"))
+                num2 = Number(prompt("ange ditt andra tal?"))
+                console.log( "x * y"+"x =",num1 ,"y =",num2 + num1 * num2)
                 break;
-                default:
-                    console.log("Du måste skriva vilken meny (1-3) du vill in på")
+                case 4:
+                    num1 = Number(prompt("ange ditt första tal?"))
+                    num2 = Number(prompt("ange ditt andra tal?"))
+                    console.log("x / y = :",num1 / num2)
                     break;
+                    case 5:
+                        num1 = Number(prompt("ange ditt första tal?"))
+                        num2 = 2
+                        console.log("ditt tal ^2 blir :",num1 ** num2)
+                        break;
+                        case 6:
+                            num1 = Number(prompt("ange ditt första tal?"))
+                            num2 = Number(prompt("ange ditt andra tal?"))
+                            console.log("x % y = :",num1 % num2)
+                            break;
+
+                default:
+                    console.log("Du måste skriva vilken meny (1-6) du vill in på")
+                break;
 
 }
