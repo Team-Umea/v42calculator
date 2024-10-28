0f6b70097f310e456f9e1009d33199f4eb27cc19
oscar <oscar.burman005@gmail.com>
2024-10-28 10:36:23 +0100

    Fixed history render bug

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // let menu = prompt("Detta är vår meny");
 // menu = trimInput(menu);
 
-let menu;
+// let menu;
+let menuChoice;
 let history = [];
 
 //Addition
@@ -28,11 +29,9 @@ const menuText = `Välj en uträkningsmetod nedan (1-11)\n
     11. Avsluta för att avsluta programmet`;
 
 do {
-
-let menuChoice;
-do{
+  do {
     menuChoice = Number(promptToNumber(menuText));
-}while(menuChoice<1||menuChoice>11);
+  } while (menuChoice < 1 || menuChoice > 11);
 
   let calc = true;
   switch (menuChoice) {
@@ -44,7 +43,7 @@ do{
         let continuePrompt = prompt(`Summan av ${num1} + ${num2} = ${sum}\n\n Om du vill gå tillbaka till menyn skriv 'ja' annars tryck enter eller ok?`);
 
         history.unshift(num1 + " + " + num2 + " = " + sum);
-        isValidInput(continuePrompt) === "ja" ? calc = false : calc = true; //ternary operator ✔
+        isValidInput(continuePrompt) === "ja" ? (calc = false) : (calc = true); //ternary operator ✔
       }
       break; //Avsluta case 1
     case 2: //subtraktion
@@ -125,7 +124,7 @@ do{
       while (calc) {
         let num1 = promptToNumber("Ange vilket tal du vill ta roten ur på:");
         let num2 = promptToNumber("Ange vilken grad av rot du vill använda (ex. 2, 3, 4...)");
-        let sum = num1**(1/num2); //num1*(1/num2)  (num1**1/2) ((num**2)*1/2) ??? ROTEN UR!! Math.pow(num1,1/num2)
+        let sum = num1 ** (1 / num2); //num1*(1/num2)  (num1**1/2) ((num**2)*1/2) ??? ROTEN UR!! Math.pow(num1,1/num2)
         let continuePrompt = prompt(`Resultatet av ${num2} √ ${num1} = ${sum}\n\n Om du vill gå tillbaka till menyn skriv 'ja' annars tryck enter eller ok?`);
         history.unshift(num1 + " √ " + num2 + " = " + sum);
         if (isValidInput(continuePrompt) === "ja") {
@@ -135,10 +134,10 @@ do{
       //log = let sum = Math.log(num1) / Math.log(num2)
       break;
     case 8: //logarithmer
-    while (calc) {
+      while (calc) {
         let num1 = promptToNumber("Ange basen för logaritmerisk beräkning (t.ex. 3):");
         let num2 = promptToNumber("Ange värdet som ska logaritmeras (t.ex. 9)");
-        let sum = Math.log(num2)/Math.log(num1);
+        let sum = Math.log(num2) / Math.log(num1);
         let continuePrompt = prompt(`log${num1}(${num2}) = ${sum}\n\n Om du vill gå tillbaka till menyn skriv 'ja' annars tryck enter eller ok?`);
         history.unshift(`log${num1}(${num2}) = ${sum}`);
         if (isValidInput(continuePrompt) === "ja") {
@@ -147,59 +146,51 @@ do{
       }
       break;
     case 9: //trignometeri
-        while (calc) {
-            let trigonometricExp = promptToTrigonometric();
-            let degOrRad = extractNumber(trigonometricExp);
-            let trigonometricFunc = extractTrigFunction(trigonometricExp);
-            let unit = trigonometricExp.includes("rad") ? "rad" : "deg";
-            let angle = unit === "rad" ? degOrRad * (Math.PI / 180) : degOrRad;
-            let calculation;
-            switch (trigonometricFunc) {
-              case "tan":
-                calculation = Math.tan(angle);
-                break;
-              case "sin":
-                calculation = Math.sin(angle);
-                break;
-              case "cos":
-                calculation = Math.cos(angle);
-                break;
-            }
-    
-            let calculationAsText = `${trigonometricFunc.charAt(0).toUpperCase() + trigonometricFunc.slice(1)} of ${degOrRad}${unit === "rad" ? "π" : "°"} = ${calculation}`;
-            history.unshift(calculationAsText)
-            let continuePrompt = prompt(`${calculationAsText}\n\n Om du vill gå tillbaka till menyn skriv 'ja' annars tryck enter eller ok?`);
+      while (calc) {
+        let trigonometricExp = promptToTrigonometric();
+        let degOrRad = extractNumber(trigonometricExp);
+        let trigonometricFunc = extractTrigFunction(trigonometricExp);
+        let unit = trigonometricExp.includes("rad") ? "rad" : "deg";
+        let angle = unit === "rad" ? degOrRad * (Math.PI / 180) : degOrRad;
+        let calculation;
+        switch (trigonometricFunc) {
+          case "tan":
+            calculation = Math.tan(angle);
+            break;
+          case "sin":
+            calculation = Math.sin(angle);
+            break;
+          case "cos":
+            calculation = Math.cos(angle);
+            break;
+        }
 
-            if (isValidInput(continuePrompt) === "ja") {
-                calc = false;
-              }
-          }
+        let calculationAsText = `${trigonometricFunc.charAt(0).toUpperCase() + trigonometricFunc.slice(1)} of ${degOrRad}${unit === "rad" ? "π" : "°"} = ${calculation}`;
+        history.unshift(calculationAsText);
+        let continuePrompt = prompt(`${calculationAsText}\n\n Om du vill gå tillbaka till menyn skriv 'ja' annars tryck enter eller ok?`);
+
+        if (isValidInput(continuePrompt) === "ja") {
+          calc = false;
+        }
+      }
       break;
     case 10: //visa historik
       let arrAsText = "";
       for (let i = 0; i < history.length; i++) {
-        arrAsText += `${i + 1}. ${history[i]}\n`;
+        arrAsText += `${i + 1}: ${history[i]}\n`;
       }
-      let continuePrompt = prompt(
-        `Här är dina uträkningar mata in korresponderande siffra för att använda den i en annan uträkning, alternativt skriv 'ja' för att gå tillbaka till huvudmenyn
-        
-        ${arrAsText}`
-      );
+      let continuePrompt = prompt("Här är dina uträkningar mata in korresponderande siffra för att använda den i en annan uträkning, alternativt skriv 'ja' för att gå tillbaka till huvudmenyn\n\n" + arrAsText);
       if (continuePrompt === "ja") {
         break;
-      } 
-      else if (
-        isNaN(continuePrompt) &&
-        0 < continuePrompt < history.length + 1
-      ) {
-        storedNum = arrAsText.slice(0 , arrAsText.lastIndexOf("=")).trim();
+      } else if (isNaN(continuePrompt) && 0 < continuePrompt < history.length + 1) {
+        storedNum = arrAsText.slice(0, arrAsText.lastIndexOf("=")).trim();
       }
       break;
 
     default: //fel inmatning, be användaren MATA OM
       break;
   }
-} while (menu !== 11);
+} while (menuChoice !== 11);
 
 //error hantering av input, tar bort blank space
 function isValidInput(input) {
@@ -207,18 +198,18 @@ function isValidInput(input) {
   return input;
 }
 
-function promptToNumber(promptMessage,isNum1) {
-    let input;
+function promptToNumber(promptMessage, isNum1) {
+  let input;
 
-    if(isNum1 && storedNum !== undefined){
-        alert(`num1 är ${storedNum}`);
-        input = parseFloat(storedNum);
-        storedNum = undefined; 
-    }else{
-        do {
-        input = parseFloat(isValidInput(prompt(promptMessage).replace(",", ".")));
-        } while (isNaN(parseFloat(input)));
-    }
+  if (isNum1 && storedNum !== undefined) {
+    alert(`num1 är ${storedNum}`);
+    input = parseFloat(storedNum);
+    storedNum = undefined;
+  } else {
+    do {
+      input = parseFloat(isValidInput(prompt(promptMessage).replace(",", ".")));
+    } while (isNaN(parseFloat(input)));
+  }
   return input;
 }
 
@@ -231,33 +222,33 @@ function promptToNumberNoZero(promptMessage) {
 }
 
 function promptToTrigonometric() {
-    let trigFunction;
-  
-    do {
-      trigFunction = prompt("Ange en trigonometriska funktion följt av antalet grader (cos90, sin10, tan270...) för att använda radianer istället för grader skriv 'rad' efter antalet grader (cos1rad, sin1.2rad, tan0.3rad...) ").trim().toLowerCase().replace(",", ".");
-    } while (!(Boolean(extractNumber(trigFunction)) && (trigFunction.includes("tan") || trigFunction.includes("sin") || trigFunction.includes("cos"))) || !Boolean(extractNumber(trigFunction)));
-  
-    return trigFunction;
-  }
-  
-  function extractTrigFunction(str) {
-    return str.includes("tan") ? "tan" : str.includes("sin") ? "sin" : str.includes("cos") ? "cos" : "";
-  }
-  
-  function extractNumber(str) {
-    let number = "";
-    let foundNumber = false;
-      for (let i = 0; i < str.length; i++) {
-        const ch = str[i];
-        if (!isNaN(ch)) {
-          foundNumber = true;
-        }
-        if (foundNumber && isNaN(ch) && ch !== ".") {
-          break;
-        }
-        if (!isNaN(ch) || ch === ".") {
-          number += ch;
-        }
+  let trigFunction;
+
+  do {
+    trigFunction = prompt("Ange en trigonometriska funktion följt av antalet grader (cos90, sin10, tan270...) för att använda radianer istället för grader skriv 'rad' efter antalet grader (cos1rad, sin1.2rad, tan0.3rad...) ").trim().toLowerCase().replace(",", ".");
+  } while (!(Boolean(extractNumber(trigFunction)) && (trigFunction.includes("tan") || trigFunction.includes("sin") || trigFunction.includes("cos"))) || !Boolean(extractNumber(trigFunction)));
+
+  return trigFunction;
+}
+
+function extractTrigFunction(str) {
+  return str.includes("tan") ? "tan" : str.includes("sin") ? "sin" : str.includes("cos") ? "cos" : "";
+}
+
+function extractNumber(str) {
+  let number = "";
+  let foundNumber = false;
+  for (let i = 0; i < str.length; i++) {
+    const ch = str[i];
+    if (!isNaN(ch)) {
+      foundNumber = true;
     }
-    return parseFloat(number);
+    if (foundNumber && isNaN(ch) && ch !== ".") {
+      break;
+    }
+    if (!isNaN(ch) || ch === ".") {
+      number += ch;
+    }
+  }
+  return parseFloat(number);
 }
